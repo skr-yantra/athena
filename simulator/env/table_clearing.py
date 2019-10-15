6a0835efff9cd2ff64560b1f1b5c2414c75fac6d
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-10-15 14:35:32 +0530

    Pass down pybullet client

--- simulator/env/table_clearing.py
+++ simulator/env/table_clearing.py
@@ -12,15 +12,15 @@ from .base import Environment
 class TableClearingEnvironment(Environment):
 
     def _setup(self):
-        pb.setGravity(0, 0, -9.81)
+        self._pb_client.setGravity(0, 0, -9.81)
 
-        self._ground = Ground()
-        self._robot = IRB120()
-        self._src_table = Table(pose=(0, -0.5, 0, 0, 0, 0), scale=0.5)
-        self._dest_table = Table(pose=(0, 0.5, 0, 0, 0, 0), scale=0.5)
+        self._ground = Ground(self._pb_client)
+        self._robot = IRB120(self._pb_client)
+        self._src_table = Table(self._pb_client, pose=(0, -0.5, 0, 0, 0, 0), scale=0.5)
+        self._dest_table = Table(self._pb_client, pose=(0, 0.5, 0, 0, 0, 0), scale=0.5)
 
-        self._src_tray = Tray(pose=(0, -0.5, self._src_table.z_end, 0, 0, 0), scale=0.5)
-        self._dest_tray = Tray(pose=(0, 0.5, self._dest_table.z_end, 0, 0, 0), scale=0.5)
+        self._src_tray = Tray(self._pb_client, pose=(0, -0.5, self._src_table.z_end, 0, 0, 0), scale=0.5)
+        self._dest_tray = Tray(self._pb_client, pose=(0, 0.5, self._dest_table.z_end, 0, 0, 0), scale=0.5)
 
         for i in range(50):
             self._src_tray.add_random_cube()
