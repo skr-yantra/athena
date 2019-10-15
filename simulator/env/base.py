70212502252425f48fb6df1402b0dbf54b83a68f
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-10-15 20:16:59 +0530

    Smooth realtime simulation

--- simulator/env/base.py
+++ simulator/env/base.py
@@ -7,17 +7,24 @@ from ..utils import unimplemented
 
 class Environment(object):
 
-    def __init__(self, pb_client=pb):
+    def __init__(self, pb_client=pb, step_size=1./240.):
         self._pb_client = pb_client
+
+        self._step_size = step_size
+        self._last_step_time = time.time_ns()
+
         self._setup()
 
     def spin(self):
         while True:
             self.step()
-            time.sleep(1./240.)
 
     def step(self):
         self._pb_client.stepSimulation()
+        current = time.time_ns()
+        elapsed = current - self._last_step_time
+        if elapsed < self._step_size:
+            time.sleep(self._step_size-elapsed)
 
     def _setup(self):
         self._step = 0
