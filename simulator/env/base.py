d1c8649dcdfd038a96a5c7d275cdeb566f37206a
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-10-15 21:48:58 +0530

    Option to run simulation in faster relative time frame

--- simulator/env/base.py
+++ simulator/env/base.py
@@ -7,11 +7,12 @@ from ..utils import unimplemented
 
 class Environment(object):
 
-    def __init__(self, pb_client=pb, step_size=1./240.):
+    def __init__(self, pb_client=pb, step_size=1./240., realtime=True):
         self._pb_client = pb_client
 
         self._step_size = step_size
         self._last_step_time = time.time_ns()
+        self._realtime = realtime
 
         self._setup()
 
@@ -23,7 +24,7 @@ class Environment(object):
         self._pb_client.stepSimulation()
         current = time.time_ns()
         elapsed = current - self._last_step_time
-        if elapsed < self._step_size:
+        if self._realtime and elapsed < self._step_size:
             time.sleep(self._step_size-elapsed)
 
     def _setup(self):
