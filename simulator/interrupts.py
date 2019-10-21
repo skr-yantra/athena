0ee84357215b04f6e898efe407eb8c1940a8524f
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-10-21 19:46:01 +0530

    Refactor and remove env dependency

--- simulator/interrupts.py
+++ simulator/interrupts.py
@@ -5,21 +5,18 @@ from simulator.utils import unimplemented
 
 class Interrupt(object):
 
-    def __init__(self, env):
-        self._env = env
-
-    def interrupt_tick(self):
+    def tick(self):
         unimplemented()
 
 
 class NumericStateInterrupt(Interrupt):
 
-    def __init__(self, env, target_state, state_reader, tolerance=1e-4):
-        super(NumericStateInterrupt, self).__init__(env)
+    def __init__(self, target_state, state_reader, tolerance=1e-4):
+        super(NumericStateInterrupt, self).__init__()
         self._state_reader = state_reader
         self._target_state = target_state
         self._tolerance = tolerance
 
-    def interrupt_tick(self):
-        current_state = self._state_reader(self._env)
+    def tick(self):
+        current_state = self._state_reader()
         return np.all(np.abs(current_state - self._target_state) <= self._tolerance)
