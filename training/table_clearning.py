213d1354cf5465b79989677190288bf0b53d5750
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-10-24 21:48:11 +0530

    Implement reward calculation

--- training/table_clearning.py
+++ training/table_clearning.py
@@ -1,13 +1,46 @@
+import numpy as np
+
 from simulator.env.table_clearing import EpisodeState
+from hparams import read_params
 
 
 class RewardCalculator(object):
 
     _s_tm1: EpisodeState
 
-    def __init__(self):
-        self._s_tm1 = None
+    def __init__(self, initial_state, params=read_params('table_clearing_base')):
+        self._s_tm1 = initial_state
+        self._params = params
 
-    def update(self, state):
-        # TODO
-        pass
+    def update(self, state: EpisodeState):
+        reward = 0.0
+
+        # Reaching
+        if not state.grasped:
+            reward += self._params.reward.reaching * np.sign(self._s_tm1.d_tg - state.d_tg)
+
+        # Delivering
+        if state.grasped:
+            reward += self._params.reward.delivering * np.sign(self._s_tm1.d_gd - state.d_gd)
+
+        # Successful grasp
+        if not self._s_tm1.grasped and state.grasped:
+            reward += self._params.reward.grasped
+
+        reached_destination = state.d_gd < 0.05
+
+        # Dropped target
+        if self._s_tm1.grasped and not state.grasped and not reached_destination:
+            reward += self._params.reward.dropped
+
+        # Reached destination
+        if self._s_tm1.grasped and not state.grasped and reached_destination:
+            reward += self._params.reward.delivered
+
+        # Collided
+        if state.collided:
+            reward += self._params.reward.collided
+
+        self._s_tm1 = state
+
+        return reward
