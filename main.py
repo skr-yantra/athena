dd4b4875ba4b2f746b04d43cc0e3b084639c8d20
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-02 20:22:10 +0530

    Split scripts

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-#!/usr/bin/env python
-import math
+import click
 
-import pybullet as pb
-from simulator.env.irb120 import IRB120
-import pybullet_data
+from env_test import main as env_test
+from train import main as train
 
 
-def _main():
-    pb.connect(pb.GUI)
-    simulator = IRB120()
+@click.group()
+def main():
+    pass
 
-    pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-    pb.loadURDF("table/table.urdf", [0.6, 0, 0], globalScaling=0.5)
-    pb.loadURDF("cube_small.urdf", [0.4, 0, 0.5], globalScaling=0.5)
 
-    simulator.release_gripper()
-    simulator.move_absolute([0.445, 0, 0.5, 0, math.pi/2, 0])
-    simulator.move_relative([0, 0, -0.14, 0, 0, 0])
-    simulator.hold_gripper()
-    simulator.move_relative([0, 0, 0.1, 0, 0, 0])
-    simulator.move_relative([0.2, 0, 0, 0, 0, 0])
-    simulator.move_relative([0, 0, -0.1, 0, 0, 0])
-    simulator.release_gripper()
-    simulator.move_relative([0, 0, 0.1, 0, 0, 0])
-    simulator.move_relative([-0.2, 0, 0, 0, 0, 0])
-    simulator.spin()
-
+main.add_command(env_test)
+main.add_command(train)
 
 if __name__ == '__main__':
-    _main()
+    main()
