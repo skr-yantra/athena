b02ef5c79c306e7c87e63f53a6caa7f1a2153587
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-03 23:24:23 +0530

    Init comet.ml experiment logging

---
+++ comet.py
@@ -0,0 +1,9 @@
+from comet_ml import Experiment
+
+
+def new_experiment():
+    return Experiment(
+        api_key="lzBiJi5UcZCwhAqlFNCUP4Qpg",
+        project_name="athena",
+        workspace="skr-io7803",
+    )
