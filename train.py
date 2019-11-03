ed516e30cb5632a88bb6d49f28e1d7172858c6c1
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-03 23:54:49 +0530

    Log training

--- train.py
+++ train.py
@@ -1,26 +1,61 @@
+from comet import new_experiment
+
+import ray
 import click
+
+from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
+from ray.tune.logger import pretty_print
 
 import envs
 
 
-def train(num_gpus=0, num_workers=1, render=False):
-    import ray
-    from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
-    from ray.tune.logger import pretty_print
-
+def train(environment='table-clearing-v0', iterations='1000', num_gpus='1',
+          num_workers='1', render='0', comet='0', save_every='10'):
     ray.init()
+
+    iterations = int(iterations)
+    save_every = int(save_every)
+    num_gpus = int(num_gpus)
+    num_workers = int(num_workers)
+    render = render == '1'
+    comet = comet == '1'
 
     config = DEFAULT_CONFIG.copy()
     config["num_gpus"] = num_gpus
     config["num_workers"] = num_workers
     config["env_config"] = {"render": render}
 
-    trainer = PPOTrainer(config=config, env='table-clearing-v0')
+    comet = new_experiment() if comet else None
 
-    while True:
-        print(pretty_print(trainer.train()))
+    trainer = PPOTrainer(config=config, env=environment)
+
+    for i in range(iterations):
+        result = trainer.train()
+        print(pretty_print(result))
+
+        if i % save_every == 0 or i == iterations-1:
+            check_point = trainer.save()
+            print('Checkpoint saved at {}'.format(check_point))
+
+            if comet is not None:
+                comet.log_asset(check_point)
+
+        if comet is None:
+            continue
+
+        comet.log_current_epoch(i)
+
+        metrics = [
+            'episode_reward_max', 'episode_reward_mean', 'episode_reward_min',
+            'episode_len_mean', 'episodes_total', 'timesteps_total'
+        ]
+
+        for metric in metrics:
+            comet.log_metric(metric, result[metric])
 
 
 @click.command('train')
-def main():
-    train()
+@click.argument('name', type=click.STRING)
+@click.option('--config', '-c', type=(str, str), multiple=True)
+def main(name, config):
+    train(name, **dict(config))
