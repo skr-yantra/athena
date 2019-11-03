0f8d61ca89ef6032f9853a46614472de34ff0082
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-03 22:47:58 +0530

    Fix time unit

--- simulator/env/base.py
+++ simulator/env/base.py
@@ -8,7 +8,7 @@ class Environment(object):
     def __init__(self, pb_client=pb, step_size=1./240., realtime=True):
         self._pb_client = pb_client
 
-        self._step_size = step_size * 10e9
+        self._step_size = step_size * 1e9
         self._last_step_time = time.time_ns()
         self._realtime = realtime
 
@@ -27,7 +27,7 @@ class Environment(object):
         to_sleep = max(0., self._step_size - elapsed)
 
         if self._realtime and to_sleep > 0:
-            time.sleep(to_sleep / 10e9)
+            time.sleep(to_sleep / 1e9)
 
         self._last_step_time = current + to_sleep
 
