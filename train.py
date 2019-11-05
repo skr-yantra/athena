6dfaf62b659ec721ef3ad8ec9b83c6d674879389
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-05 23:57:02 +0530

    Auto object memory

--- train.py
+++ train.py
@@ -14,9 +14,7 @@ import envs
 
 def train(environment='table-clearing-v0', iterations='1000', num_gpus='1',
           num_workers='1', render='0', comet='0', save_frequency='10'):
-    ray.init(
-         object_store_memory=5*1000*1000*1000
-    )
+    ray.init()
 
     iterations = int(iterations)
     save_frequency = int(save_frequency)
