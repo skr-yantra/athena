40f6260e3396b38ac52b294e65b89bcae05a789d
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-08 23:49:32 +0530

    Add setup script for Ubuntu 18.04 cloud image

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Upgrade ubuntu
+sudo apt update
+sudo apt upgrade
+
+# Install CUDA
+bash setup_cuda.sh
+
+# Install build deps
+sudo apt install libpython3-dev python3-venv
+
+# Create virtual env
+python3 -m venv ~/.env
+
+# Auto source to .env
+echo "source ~/.env/bin/activate" >> ~/.bashrc
+
+# Reboot
+sudo reboot
