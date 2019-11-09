1651500bb83120b2111d0d22019a9c2473d95c3c
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-09 13:37:18 +0530

    Reduce SVGG filter size

--- models/svgg_v1.py
+++ models/svgg_v1.py
@@ -8,14 +8,14 @@ class SimpleVisionNet(VGGNet):
 
     def _make_conv_layer_builders(self):
         return [
-            self._conv(32, 'conv1'),
+            self._conv(16, 'conv1'),
             self._pool('pool1'),
-            self._conv(64, 'conv2'),
+            self._conv(32, 'conv2'),
             self._pool('pool2'),
-            self._conv(128, 'conv3'),
+            self._conv(48, 'conv3'),
             self._pool('pool3'),
-            self._conv(256, 'conv4'),
+            self._conv(64, 'conv4'),
             self._pool('pool4'),
-            self._conv(512, 'conv5'),
+            self._conv(80, 'conv5'),
             self._pool('pool5'),
         ]
