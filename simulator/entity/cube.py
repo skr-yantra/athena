cbaf4b6752b24801cc8dfcc1626405cb71fa2ce9
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-12 21:29:07 +0530

    Minor shrinking

--- simulator/entity/cube.py
+++ simulator/entity/cube.py
@@ -8,7 +8,7 @@ from .base import Entity
 
 class Cube(Entity):
 
-    def __init__(self, pb_client=pb, position=(0, 0, 0), orientation=(0, 0, 0, 1), fix_base=False, scale=0.5, **kwargs):
+    def __init__(self, pb_client=pb, position=(0, 0, 0), orientation=(0, 0, 0, 1), fix_base=False, scale=0.4, **kwargs):
         table = os.path.join(pybullet_data.getDataPath(), 'cube_small.urdf')
         super(Cube, self).__init__(table, pb_client, position, orientation, fix_base, scale, **kwargs)
         self._pb_client.changeDynamics(
