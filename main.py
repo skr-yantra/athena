fba623f0e075515d5d7cfb964f760ab3b0a4f73e
Sreejith Krishnan R <sreejithkrishnan.ip@gmail.com>
2019-11-20 17:28:49 +0530

    Fix comet initialization error

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import click
 
+from train import main as train
 from env_test import main as env_test
 from env_benchmark import main as env_benchmark
 from env_sampler import main as env_sampler
-from train import main as train
 from evaluate import main as evaluate
 
 
